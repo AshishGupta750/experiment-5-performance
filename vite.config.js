dacc7da1dd0f83c61e7f5c0460c32f83bc7bb0fa
Ashish Kumar Gupta <23BAI70663@cuchd.in>
2026-02-24 23:28:58 +0530

    Fix react-window export error

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer'; // [cite: 259]
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // Generates stats.html to analyze bundle size [cite: 263]
-    visualizer({ open: true }) 
+    visualizer({ open: true })
   ],
+  resolve: {
+    alias: {
+      // FIX: Force react-window to use the ES Module build to solve "Missing Export" error
+      'react-window': 'react-window/dist/index.esm.js',
+    },
+  },
 })
